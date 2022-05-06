59d2ca8a2f40dac22a8e18f71530ac10455f30fa
Andres Benitez <spartansup18@gmail.com>
2022-05-05 17:17:05 -0700

    enemy class name

--- src/prefab/enemy.js
+++ src/prefab/enemy.js
@@ -1,4 +1,4 @@
-class dude extends Phaser.Physics.Arcade.Sprite{
+class enemy extends Phaser.Physics.Arcade.Sprite{
     constructor(scene, x, y, texture) {
         super(scene, x, y, texture);
         scene.add.existing(this);
