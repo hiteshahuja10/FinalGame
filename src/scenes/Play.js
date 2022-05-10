d60cd9b0e25df6e35269d39dde008115f3892cd5
Nicholas <nklau@ucsc.edu>
2022-05-09 19:15:17 -0700

    test tilemap

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -8,6 +8,8 @@ class Play extends Phaser.Scene {
         this.load.image('tile', './assets/Tile.png');
         this.load.image('platform', './assets/StonePlatform.png');
         this.load.image('player', './assets/Player.png');
+        this.load.image('floor','./assets/ground.png')
+        this.load.tilemapTiledJSON('tilemap','./assets/Test_Map.json')
         /*this.load.image('spike','./assets/Spikes.png');
         this.load.image('spike1','./assets/Spikes1.png');
         this.load.image('downspike','./assets/Spike_Down.png');
@@ -18,10 +20,17 @@ class Play extends Phaser.Scene {
         this.load.spritesheet('leftrun', './assets/Player_LeftRun.png',{frameWidth:53, frameHeight:75, startFrame:0, endFrame:4});
         this.load.spritesheet('rightrun', './assets/Play_RightRun.png',{frameWidth:53, frameHeight:75, startFrame:0, endFrame:4});
         this.load.spritesheet('vibing', './assets/Player.png',{frameWidth:53, frameHeight:75, startFrame:0, endFrame:0} )*/
+
+        
+
     }
 
     create() {
+        const map = this.make.tilemap({key: 'tilemap'})
+        const tileset = map.addTilesetImage('castle-ground','floor')
         this.tile = this.add.tileSprite(0, 0, 560, 700, 'tile').setOrigin(0, 0);
+
+        const ground = map.createLayer('ground',tileset);
 
         this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
         this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 
