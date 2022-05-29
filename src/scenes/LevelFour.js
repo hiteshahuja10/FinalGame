643c9d4b4df0b7c3ffaf44a0aa623cfe530cf4fe
Nicholas <nklau@ucsc.edu>
2022-05-28 21:30:49 -0700

    map 4

---
+++ src/scenes/LevelFour.js
@@ -0,0 +1,67 @@
+class Three extends Phaser.Scene {
+    constructor(){
+        super("levelThree");
+        this.enemy;
+    }
+
+    preload(){
+        this.load.image('tile', './assets/back2.png');
+        this.load.image('sep', './assets/sep1.png');
+        this.load.image('sep2', './assets/sep2.png');
+        this.load.image('ground', './assets/ground2.png');
+        this.load.image('new', './assets/new1.png');
+        this.load.image('plat_form', './assets/Platform.png');
+        this.load.image('player', './assets/knight.png');
+        this.load.image('floor','./assets/ground.png')
+        this.load.image('wall','./assets/walls.png')
+        this.load.image('line', './assets/line.png');
+        this.load.image('line2', './assets/outline.png');
+        this.load.image('sword1', './assets/SwordPiece_1.png');
+        this.load.image('torch', './assets/torch2.png');
+        this.load.image('heart', './assets/heart.png');
+        this.load.image('enemy', './assets/enemy.png');
+        this.load.image('slash', './assets/slash.png');
+        this.load.image('swordbar', './assets/swordbar2.png');
+        this.load.image('collectone', './assets/collectone.png');
+        this.load.image('spike','./assets/spikes.png');
+        this.load.image('spike2','./assets/double_spike2.png');
+        this.load.image('spike3','./assets/backspike2.png');
+        this.load.image('healthbar', './assets/healthbar.png');
+        this.load.image('healthbar2', './assets/healthbar2.png');
+        this.load.image('healthbar3', './assets/healthbar3.png');
+        this.load.spritesheet('enemy1', './assets/enemy_attack.png',{frameWidth:32, frameHeight:32, startFrame:0, endFrame:5});
+        this.load.spritesheet('run_right','./assets/Player_Run.png',{frameWidth:52, frameHeight:80, startFrame:0, endFrame:10});
+        this.load.spritesheet('run_left','./assets/Player_Run_Left.png',{frameWidth:52, frameHeight:80, startFrame:0, endFrame:10});
+        this.load.spritesheet('jump_right','./assets/Jump_Right.png',{frameWidth:52, frameHeight:80, startFrame:0, endFrame:0});
+        this.load.spritesheet('jump_left','./assets/Jump_Left.png',{frameWidth:52, frameHeight:80, startFrame:0, endFrame:0});
+        this.load.spritesheet('vibing','./assets/knight.png',{frameWidth:52, frameHeight:80, startFrame:0, endFrame:0});
+        this.load.spritesheet('vibingL','./assets/knight_left.png',{frameWidth:52, frameHeight:80, startFrame:0, endFrame:0});
+        this.load.spritesheet('SlashAni','./assets/Slash_Ani.png',{frameWidth:52, frameHeight:40, startFrame:0, endFrame:10});
+        this.load.spritesheet('SlashAniL','./assets/Slash_Ani_Left.png',{frameWidth:39, frameHeight:39, startFrame:0, endFrame:10});
+
+
+
+        //map Assets
+        this.load.image('sep', './assets/sep1.png');
+        this.load.image('heart', './assets/heart.png');
+        this.load.image('tile_four', './assets/stage4atlas.png');
+        this.load.tilemapTiledJSON('tilemap3','./assets/test_map3.json');
+    }
+
+    create(){
+        //this.music.stop();
+        this.music = this.sound.add('sfx_music');
+        this.music.loop = true;
+        this.music.play();
+        this.spike = this.physics.add.staticGroup();
+        //this.scale.updateBounds(1632, 720);
+        //this.scale.setGameSize(850, 1000);
+
+        //map loading
+        const map = this.make.tilemap({key: 'tilemap3'})
+        const tileset_four = map.addTilesetImage('tileset','tile_four');
+        const bg = map.createLayer('background3', tileset_four);
+        this.ground = map.createLayer('Ground3',tileset_three);
+        this.ground.setCollisionByProperty({collides: true})
+    }
+}
